ef8a9b0962d08afb6e8b7a1ee5558e1b148413fc
imhiju7 <imhiju7@gmail.com>
2023-12-01 19:27:44 +0700

    update

--- src/GUI/comp/Event.java
+++
@@ -1,9 +0,0 @@
-package GUI.comp;
-
-import com.raven.datechooser.*;
-import java.awt.event.MouseEvent;
-
-public interface Event {
-
-    public void execute(MouseEvent evt, int num);
-}
--- src/GUI/comp/EventDateChooser.java
+++
@@ -1,8 +0,0 @@
-package GUI.comp;
-
-import com.raven.datechooser.*;
-
-public interface EventDateChooser {
-
-    public void dateSelected(SelectedAction action, SelectedDate date);
-}
