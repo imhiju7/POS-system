4b7c39c4eb5e4cc79f13a517ffc2a0da0623f8b9
ADMIN <luanvd003@gmail.com>
2023-12-01 20:44:13 +0700

    edit

--- src/GUI/GUIDangNhap.java
+++ src/GUI/GUIDangNhap.java
@@ -244,7 +244,7 @@ public class GUIDangNhap extends javax.swing.JFrame {
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
-      //  dangNhap();
+//        dangNhap();
     }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
